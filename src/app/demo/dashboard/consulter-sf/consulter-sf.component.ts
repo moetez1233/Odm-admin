66056e7af5f13d5c52bf123157466a7d13c91d6d
unknown <moetezmaddouri@gmail.com>
2022-04-12 12:17:24 +0200

    Sprint 4 version final

--- src/app/demo/dashboard/consulter-sf/consulter-sf.component.ts
+++ src/app/demo/dashboard/consulter-sf/consulter-sf.component.ts
@@ -11,25 +11,48 @@ export class ConsulterSfComponent implements OnInit {
 shipmentFile:ShipmentFile[]
 clickedSf:ShipmentFile =new ShipmentFile()
 adminIndex = false;
+DragDrop=true;
+ISImportAbord=false;
 SfColor = []
-
+ShowDetails=false
 
   constructor(private consulterSfService:ConsulterSfService) { }
 
   ngOnInit() {
-    this. AllSF()
+    this.AllSF()
+    console.log("hello");
+    
+    setInterval(()=>{this. AllSF(); console.log("hello");},1000)
   }
   AllSF(){
     this.consulterSfService.getAllSf().subscribe( (res)=>{
      this.shipmentFile=res as ShipmentFile[];
-     // console.log(this.shipmentFile[0].name);
+      console.log(this.shipmentFile[0].user.name);
       
       }  )
   }
   ShipmentFileClicked(index){
+    this.ShowDetails=true;
     this.clickedSf=this.shipmentFile[index];
+    this.ISImportAbord=false;
+    console.log(this.clickedSf.user);
+    
+    if(this.clickedSf.status=="Import-Aborded"){
+      this.ISImportAbord=true;
+    }
+   if(this.clickedSf.user){
+   
+    this.DragDrop=true;
+   
+   }else{
+    this.DragDrop=false;
+   }
+    
     console.log(this.clickedSf.historiques[0].idHistorique.modefication_sf);
     
+  }
+  ReturnToList(){
+    this.ShowDetails=false;
   }
 
   hoverOn(adminIndex) {
@@ -44,11 +67,21 @@ SfColor = []
       this.SfColor[adminIndex] = "light"
     }
   }
+
+  
   cherchStatus(status){
+    this.ISImportAbord=false;
+    if(status.value=="tous"){
+      this. AllSF()
+   
+    }
+  
+    //this.ISImportAbord=false;
    this.consulterSfService.getSfByStatus(status.value).subscribe(res=>{
     this.shipmentFile=res as ShipmentFile[];
    })
    
   }
+ 
 
 }
